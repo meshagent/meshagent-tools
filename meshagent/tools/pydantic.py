2a01997e1897222e665afe89d7fced48727e5aa2
Jesse Ezell <jesse.ezell@timu.com>
2025-06-28 23:10:46 -0700

    unify bake base layer, switch to uv pip, loosen deps

--- meshagent/tools/pydantic.py
+++ meshagent/tools/pydantic.py
@@ -37,17 +37,6 @@ class PydanticTool[TInput:BaseModel](Tool):
         pass
 
 
-# -------
-
-import pydantic_ai 
-
-def get_pydantic_ai_tool_definition(*, tool: Tool) -> pydantic_ai.tools.ToolDefinition:
-    tool_definition = pydantic_ai.tools.ToolDefinition(
-        name=tool.name,
-        description=tool.description,
-        parameters_json_schema=tool.input_schema
-    )
-    return tool_definition
 
 
 
