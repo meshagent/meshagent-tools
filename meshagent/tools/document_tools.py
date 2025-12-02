c5dbbfc4db89edb978f8bbd7382b01166c688ec7
Jesse Ezell <jesse.ezell@timu.com>
2025-12-01 21:59:37 -0800

    luau

--- meshagent/tools/document_tools.py
+++ meshagent/tools/document_tools.py
@@ -34,7 +34,7 @@ class RootInsertTool(Tool):
         tag_name = element.tag_name
 
         super().__init__(
-            name=f"{document_type}_insert_{tag_name}",
+            name=f"{document_type}_insert_root_{tag_name}",
             title=f"insert {tag_name} into a {document_type} (only allowed for documents with .{document_type} extension)",
             description=f"insert a {tag_name} element at the root",
             input_schema=input_schema,
@@ -49,6 +49,65 @@ class RootInsertTool(Tool):
 
         document = documents[path]
         element = document.root.append_json(kwargs)
+        return TextResponse(text=f"The content was inserted with the id: {element.id}")
+
+
+class ElementInsertTool(Tool):
+    def __init__(self, *, document_type: str, schema: MeshSchema, element: ElementType):
+        self.element = element
+
+        tag_name = element.tag_name
+
+        cloned_element = element.from_json(element.to_json())
+
+        # remove the child props, those can be inserted in another call
+
+        child_props = []
+        for prop in cloned_element.properties:
+            if isinstance(prop, ChildProperty):
+                child_props.append(prop)
+
+        for cp in child_props:
+            cloned_element.properties.remove(cp)
+
+        input_schema = merge(
+            schema={
+                **cloned_element.to_json(),
+            },
+            additional_properties={
+                "path": {
+                    "type": "string",
+                    "description": "the id of a parent element to insert the node under",
+                },
+                "parent_element_id": {
+                    "type": "string",
+                    "description": "the path of the document to insert the element into",
+                },
+            },
+        )
+
+        super().__init__(
+            name=f"{document_type}_insert_{tag_name}",
+            title=f"insert {tag_name} into a {document_type} (only allowed for documents with .{document_type} extension)",
+            description=f"insert a {tag_name} element at the root",
+            input_schema=input_schema,
+        )
+
+    async def execute(
+        self, *, context: ToolContext, path: str, parent_element_id: str, **kwargs
+    ):
+        documents = context.room.sync.get_open_documents()
+
+        if path not in documents:
+            raise RoomException(f"the document is not currently open: {path}")
+
+        document = documents[path]
+        if parent_element_id == "root":
+            element = document.root.append_json(kwargs)
+        else:
+            element = document.root.get_node_by_id(parent_element_id).append_json(
+                kwargs
+            )
         return TextResponse(text=f"The content was inserted with the id: {element.id}")
 
 
@@ -170,16 +229,33 @@ class GetDocumentJSONTool(Tool):
 def build_tools(schema: MeshSchema, document_type: str):
     tools = list[Tool]()
 
-    for prop in schema.root.properties:
-        if isinstance(prop, ChildProperty):
-            child_type: ChildProperty = prop
-            for tag_name in child_type.child_tag_names:
-                element = schema.elements_by_tag_name[tag_name]
-                tools.append(
-                    RootInsertTool(
-                        document_type=document_type, schema=schema, element=element
-                    )
-                )
+    # for prop in schema.root.properties:
+    #    if isinstance(prop, ChildProperty):
+    #        child_type: ChildProperty = prop
+    #        for tag_name in child_type.child_tag_names:
+    #            element = schema.elements_by_tag_name[tag_name]
+    #            tools.append(
+    #                RootInsertTool(
+    #                    document_type=document_type, schema=schema, element=element
+    #                )
+    #            )
+
+    insert_tools = []
+    for element in schema.elements:
+        for prop in element.properties:
+            if isinstance(prop, ChildProperty):
+                child_type: ChildProperty = prop
+                for tag_name in child_type.child_tag_names:
+                    element = schema.elements_by_tag_name[tag_name]
+                    if tag_name not in insert_tools:
+                        insert_tools.append(tag_name)
+                        tools.append(
+                            ElementInsertTool(
+                                document_type=document_type,
+                                schema=schema,
+                                element=element,
+                            )
+                        )
 
     return tools
 
